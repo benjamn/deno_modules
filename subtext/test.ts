be99ed950093c3926ea2b7f342b1911178f33088
Ben Newman <ben@benjamn.com>
2021-04-25 20:51:20 -0400

    Export Key<T> interface type.

--- subtext/test.ts
+++ subtext/test.ts
@@ -4,7 +4,7 @@ import {
   assertNotStrictEquals,
 } from "https://deno.land/std@0.93.0/testing/asserts.ts";
 
-import { Subtext, current } from "./mod.ts";
+import { Subtext, Key, current } from "./mod.ts";
 
 Deno.test("Subtext and subtext", function () {
   assertEquals(current instanceof Subtext, true);
